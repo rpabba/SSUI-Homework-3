daa068988a0c18a74c7d6932229365cb84be6e4c
rpabba <31490818+rpabba@users.noreply.github.com>
2017-10-08 14:40:41 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function deleteItem() {
+    document.getElementById("demo").innerHTML = "SHOPPING CART (6)";
+}
+
+function addItem() {
+    document.getElementById("demo").innerHTML = "SHOPPING CART (1)";
+}
